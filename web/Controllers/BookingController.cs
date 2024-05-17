6750d0a1aafcb037ffe771d8b35e3e30e66a87c0
DavidBojanic <db5697@student.uni-lj.si>
2024-05-17 19:11:39 +0200

    Review omogucen samo ako si bukirao radnika, i sitni fixovi

--- web/Controllers/BookingController.cs
+++ web/Controllers/BookingController.cs
@@ -22,34 +22,30 @@ namespace web.Controllers
         [HttpPost]
         public async Task<IActionResult> Book(int workerId, DateTime bookingDate, TimeSpan bookingTime, string paymentMethod)
         {
-            // Retrieve the worker from the database based on the provided workerId
-            var worker = await _context.Worker.FindAsync(workerId); // Note: Ensure the DbSet is named Workers
+            var worker = await _context.Worker.FindAsync(workerId);
 
             if (worker == null)
             {
-                // If the worker doesn't exist, handle the error accordingly
                 TempData["ErrorMessage"] = "Worker not found.";
-                return RedirectToAction("Index", "Search"); // Adjust redirection as necessary
+                return RedirectToAction("Index", "Search");
             }
 
-            // Now that you have the worker details, you can proceed with creating the booking
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier); // Assuming you're using ASP.NET Core Identity
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
             var booking = new Booking
             {
                 WorkerID = workerId,
-                UserID = userId, // Use the currently logged-in user's ID
+                UserID = userId,
                 BookingDate = bookingDate,
                 BookingTime = bookingTime,
                 PaymentMethod = paymentMethod
             };
 
-            // Add the booking to the context and save changes
             _context.Bookings.Add(booking);
             await _context.SaveChangesAsync();
 
             TempData["SuccessMessage"] = "Booking successful!";
-            return RedirectToAction("Index", "Search"); // Redirect back to search results or another appropriate page
+            return RedirectToAction("Index", "Search");
         }
 
         // GET: Booking/MyBookings
@@ -57,7 +53,6 @@ namespace web.Controllers
         {
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-            // Retrieve the bookings for the currently logged-in user
             var bookings = await _context.Bookings
                 .Include(b => b.Worker)
                 .ThenInclude(w => w.Job)
@@ -65,6 +60,25 @@ namespace web.Controllers
                 .ToListAsync();
 
             return View(bookings);
+        }
+
+        // POST: Booking/Delete/5
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var booking = await _context.Bookings.FindAsync(id);
+
+            if (booking == null)
+            {
+                TempData["ErrorMessage"] = "Booking not found.";
+                return RedirectToAction(nameof(MyBookings));
+            }
+
+            _context.Bookings.Remove(booking);
+            await _context.SaveChangesAsync();
+
+            TempData["SuccessMessage"] = "Booking deleted successfully!";
+            return RedirectToAction(nameof(MyBookings));
         }
     }
 }
---
+++ web/Migrations/20240517162114_AddUserIdToReview.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace web.Migrations
+{
+    public partial class AddUserIdToReview : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Review",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Review_UserId",
+                table: "Review",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Review_ApplicationUser_UserId",
+                table: "Review",
+                column: "UserId",
+                principalTable: "ApplicationUser",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Review_ApplicationUser_UserId",
+                table: "Review");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Review_UserId",
+                table: "Review");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Review");
+        }
+    }
+}
